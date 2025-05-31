4f1b8356c88f6d4750e653a34e4aab1d24d993f6
pluu <tonplu110@gmail.com>
2025-05-31 15:44:39 +0700

    authentication update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
-import PrivateRoute from "./components/PrivateRoute";
+import PrivateRoute from "./assets/PrivateRoute";
 import Navbar from "./assets/Navbar.jsx";
 
 function App() {
