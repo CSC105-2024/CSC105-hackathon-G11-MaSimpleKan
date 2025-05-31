ad77ad11c5b3b908f7afac94a17614f429791fe1
pluu <tonplu110@gmail.com>
2025-05-31 10:04:38 +0700

    frontend homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {Outlet} from "react-router-dom";
 import Navbar from "./assets/Navbar.jsx";
 
 function App() {
     return (
         <>
-            <Navbar/>
-            <Outlet/>
+            <Navbar />
         </>
     );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,23 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import HomePage from "./pages/HomePage.jsx";
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <RouterProvider router={router} />
-    </StrictMode>
-);
+import HomePage from "./pages/HomePage.jsx";
+// import ProfilePage from "./pages/ProfilePage.jsx";
+// import SignUpPage from './pages/SignUpPage';
+// import LoginPage from './pages/LoginPage';
 
 const router = createBrowserRouter([
     {
-        path: "/", // Home route
-        element: <HomePage />, // Render the App component
-    },
-    {
-        path: "/home",
+        path: "/",
         element: <HomePage />,
     },
+    // {
+    //     path: "/profile",
+    //     element: <ProfilePage />,
+    // },
+    // {
+    //     path: "/signup",
+    //     element: <SignUpPage />,
+    // },
+    // {
+    //     path: "/login",
+    //     element: <LoginPage />,
+    // },
 ]);
 
 createRoot(document.getElementById('root')).render(
